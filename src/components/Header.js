31b6d40355d5b60b5bfe6ff13d0523eb6d041de2
Orealba Soriano <orealbas@gmail.com>
2023-06-06 18:58:37 +0200

    creating the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../components/Header.css'
 import headerBackground from '../images/headerBackground.jpg'
+import Navbar from './Navbar.js'
 
 const Header = () => {
   return (
     <div className="container-fluid">
+      <div className="header__navbar"></div>
+      <Navbar />
+      <div className="header__tittle">OREALBA SORIANO</div>
       <div
         className="header__background"
         style={{ backgroundImage: `url(${headerBackground})` }}
