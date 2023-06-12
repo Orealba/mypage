f987460765abbd98ab16d3e1a662c37202c116aa
Orealba Soriano <orealbas@gmail.com>
2023-06-12 17:39:40 +0200

    creating the profile Who I am

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 
 import Header from './components/Header'
+import Profile from './components/Profile'
 
 function App() {
   return (
     <div>
       <Header />
+      <Profile />
     </div>
   )
 }
