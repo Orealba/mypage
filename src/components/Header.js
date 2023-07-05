460c119879c0b2a2b00ac67854be74868f2167d1
Orealba Soriano <orealbas@gmail.com>
2023-07-05 12:59:23 +0200

    changes in the query

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,16 @@ const Header = () => {
   return (
     <div>
       <section
-        className="lg:bg-contain sm:bg-contain  header__background"
+        className="lg:bg-auto  sm:bg-contain  md:bg-contain bg-no-repeat header__background"
         style={{ backgroundImage: `url(${headerBackground})` }}
       >
         <div className=" header__navbar">
           <Navbar />
         </div>
-        <div className="sm:text-4xl sm:text-start md:text-6xl lg:text-8xl xl:text-8xl  header__tittle">
+        <div className="sm:text-4xl  sm:text-start md:text-6xl md:pl-20  lg:text-8xl lg:pt-60 lg:pl-20 xl:text-8xl xl:pt-60 xl:pl-20 header__tittle">
           OREALBA SORIANO
         </div>
-        <div className="sm:text-xl sm:text-start md:text-3xl  lg:text-6xl xl:text-6xl xl:text-left header__description">
+        <div className="sm:text-xl sm:text-start md:text-3xl pl-52 lg:text-6xl xl:text-6xl xl:text-left header__description">
           FULL-STACK DEVELOPER
         </div>
       </section>
