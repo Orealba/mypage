a3daadd21f0962b6cedbf49c14ad6427f1f75341
Orealba Soriano <orealbas@gmail.com>
2023-07-17 13:14:59 +0200

    continue the media q

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,16 +7,16 @@ const Header = () => {
   return (
     <div>
       <section
-        className="lg:bg-auto  sm:bg-contain  md:bg-contain bg-no-repeat header__background"
+        className=" sm:bg-contain  md:bg-contain  lg:bg-auto  bg-no-repeat header__background"
         style={{ backgroundImage: `url(${headerBackground})` }}
       >
-        <div className=" header__navbar">
+        {/* <div className=" header__navbar">
           <Navbar />
-        </div>
-        <div className="sm:text-4xl  sm:text-start md:text-6xl md:pl-20  lg:text-8xl lg:pt-60 lg:pl-20 xl:text-8xl xl:pt-60 xl:pl-20 header__tittle">
+        </div> */}
+        <div className="   sm:text-6xl  sm:text-start  md:text-6xl md:pl-20  lg:text-6xl lg:pt-60 lg:pl-20 lg:text-start xl:text-8xl xl:pt-60 xl:pl-20 xl:text-start header__tittle">
           OREALBA SORIANO
         </div>
-        <div className="sm:text-xl sm:text-start md:text-3xl pl-52 lg:text-6xl xl:text-6xl xl:text-left header__description">
+        <div className="sm:text-3xl sm:text-start md:text-3xl pl-52 lg:text-4xl xl:text-6xl xl:text-left header__description">
           FULL-STACK DEVELOPER
         </div>
       </section>
