8ccc738ea2a54e092914121973ba88e1b58ad6d8
Orealba Soriano <orealbas@gmail.com>
2023-07-19 12:52:13 +0200

    changes in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,8 @@ import '../components/Navbar.css'
 const Navbar = () => {
   return (
     <div>
-      <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+      <nav className="pl-16">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="#" className="flex items-center">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8 mr-3"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
-            </span>
-          </a>
           <button
             data-collapse-toggle="navbar-solid-bg"
             type="button"
@@ -48,17 +38,14 @@ const Navbar = () => {
               <li>
                 <a
                   href="#"
-                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                  className="block   text-red-800 "
                   aria-current="page"
                 >
                   Home
                 </a>
               </li>
               <li>
-                <a
-                  href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
+                <a href="#" className="block  text-red-800 ">
                   Services
                 </a>
               </li>
