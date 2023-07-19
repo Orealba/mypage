242ceb4c9c1318c67fd1bf39bda973eed9de2781
Orealba Soriano <orealbas@gmail.com>
2023-07-19 12:34:11 +0200

    changes in the head

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,20 +6,24 @@ import Navbar from './Navbar.js'
 const Header = () => {
   return (
     <div>
-      <section
-        className=" sm:bg-contain  md:bg-contain  lg:bg-auto  bg-no-repeat header__background"
+      <div
+        className="flex sm:bg-contain  md:bg-contain  lg:bg-contain xl:bg-contain 2xl:bg-auto bg-no-repeat header__background"
         style={{ backgroundImage: `url(${headerBackground})` }}
       >
         <div className=" header__navbar">
           <Navbar />
         </div>
-        <div className="   sm:text-6xl  sm:text-start  md:text-6xl md:pl-20  lg:text-6xl lg:pt-60 lg:pl-20 lg:text-start xl:text-8xl xl:pt-60 xl:pl-20 xl:text-start header__tittle">
-          OREALBA SORIANO
+        <div>
+          <h1 className="sm:text-2xl sm:text-start  md:text-6xl md:pl-20  lg:text-8xl lg:pt-60 lg:pl-20 lg:text-start xl:text-6xl xl:pt-60 xl:pl-20 xl:text-start header__tittle">
+            OREALBA SORIANO
+          </h1>
         </div>
-        <div className="sm:text-3xl sm:text-start md:text-3xl pl-52 lg:text-4xl xl:text-6xl xl:text-left header__description">
-          FULL-STACK DEVELOPER
+        <div>
+          <h2 className="sm:text-3xl sm:text-start md:text-3xl pl-52 lg:text-4xl xl:text-4xl xl:text-left header__description">
+            FULL-STACK DEVELOPER
+          </h2>
         </div>
-      </section>
+      </div>
     </div>
   )
 }
