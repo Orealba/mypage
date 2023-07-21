2cc7f721b1742fce6f4638230c1cfd7cdbd17e7d
Orealba Soriano <orealbas@gmail.com>
2023-07-21 13:34:20 +0200

    change the image  background en media query

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,19 +7,19 @@ const Header = () => {
   return (
     <div>
       <div
-        className="flex sm:bg-contain   md:bg-contain  lg:bg-contain xl:bg-contain 2xl:bg-auto bg-no-repeat header__background"
+        className="flex bg-contain  sm:bg-contain md:bg-contain  lg:bg-contain xl:bg-contain 2xl:bg-auto bg-no-repeat header__background"
         style={{ backgroundImage: `url(${headerBackground})` }}
       >
         <div className=" header__navbar">
           <Navbar />
         </div>
         <div>
-          <h1 className="sm:text-3xl sm:pl-14  sm:text-start  md:text-5xl md:text-start md:pl-20 md:pt-20 lg:text-7xl lg:pt-60 lg:pl-16 lg:text-start xl:text-8xl  xl:text-start header__tittle">
+          <h1 className="text-3xl pl-14  text-start  md:text-5xl md:text-start md:pl-20 md:pt-20 lg:text-7xl lg:pt-60 lg:pl-16 lg:text-start xl:text-8xl  xl:text-start header__tittle">
             OREALBA SORIANO
           </h1>
         </div>
         <div>
-          <h2 className="sm:text-md sm:text-start sm:pl-16 md:text-2xl md:text-start  md:pl-20 lg:text-4xl lg:text-start xl:text-5xl xl:text-start  header__description">
+          <h2 className="text-md text-start pl-16 md:text-2xl md:text-start  md:pl-20 lg:text-4xl lg:text-start xl:text-5xl xl:text-start  header__description">
             FULL-STACK DEVELOPER
           </h2>
         </div>
