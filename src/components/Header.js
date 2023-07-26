7c9882852b2937687fe5ddcb708e957785608e20
Orealba Soriano <orealbas@gmail.com>
2023-07-26 16:27:14 +0200

    changed the configuration with the colors and fonts, also the styles in css and js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import '../components/Header.css'
-import headerBackground from '../images/headerBackground.jpg'
+import headerBackground from '../images/headerBackground.webp'
 import Navbar from './Navbar.js'
 
 const Header = () => {
   return (
     <div>
       <div
-        className="flex bg-no-repeat bg-contain sm:bg-contain md:bg-contain  lg:bg-contain xl:bg-contain 2xl:bg-contain  header__background"
+        className="flex bg-no-repeat bg-contain header__background"
         style={{ backgroundImage: `url(${headerBackground})` }}
       >
-        <div className="header__navbar">
+        <div className="mt-8">
           <Navbar />
         </div>
         <div>
-          <h1 className="text-3xl pl-14 font-alata text-start  md:text-5xl md:text-start md:pl-20 md:pt-20 lg:text-7xl lg:pt-60 lg:pl-16 lg:text-start xl:text-8xl  xl:text-start xl:mt-28 header__tittle">
+          <h1 className="text-3xl text-myWine  pl-14 font-alata text-start  md:text-5xl md:text-start md:pl-20 md:pt-20 lg:text-7xl lg:pt-60 lg:pl-16 lg:text-start xl:text-8xl  xl:text-start xl:mt-28">
             OREALBA SORIANO
           </h1>
         </div>
         <div>
-          <h2 className="text-md text-start pl-16 md:text-2xl md:text-start  md:pl-20 lg:text-4xl lg:text-start xl:text-5xl xl:text-start  header__description">
+          <h2 className="text-md text-start font-alata text-myGrey pl-16 md:text-2xl md:text-start  md:pl-20 lg:text-4xl lg:text-start xl:text-5xl xl:text-start">
             FULL-STACK DEVELOPER
           </h2>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    colors: {
+      myWine: '#850808',
+      myGrey: '#5d534a',
+    },
     extend: {
       fontFamily: {
         alata: ['Alata'],
