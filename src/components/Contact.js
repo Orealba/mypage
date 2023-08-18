f7e196e93f4438c91c57596f8505ca22aa842519
Orealba Soriano <orealbas@gmail.com>
2023-08-18 13:34:38 +0200

    changing the form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,82 +1,81 @@
-import React from "react";
+import React from 'react'
 
 const Contact = () => {
   return (
     <div className="bg-mybG shadow-myShadow">
       <div className="container mx-auto">
         <div>
-        <h4 className="text-center pt-10 font-semibold  sm:text-5xl text-myGrey">
-          Shall we have a coffee?
-        </h4>
-        <div className="border-solid border-2 border-myWine" >
-          <div>
- <section class="bg-white dark:bg-gray-900">
-            <div class=" lg:py-16 px-4 mx-auto max-w-screen-md">
-              
-              
-              <form action="#" class="space-y-8">
-                <div>
-                  <label
-                    for="email"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          <h4 className="text-center pt-10 font-semibold  sm:text-5xl text-myGrey">
+            Shall we have a coffee?
+          </h4>
+          <div className="border-solid border-2 border-myWine my-12 mx-12">
+            <div className=" ">
+              <section class=" bg-white dark:bg-gray-900">
+                <div class=" lg:py-16 px-56 mx-auto max-w-screen-md">
+                  <form
+                    action="#"
+                    class="border-solid border-2 border-myWine py-8 px-8 space-y-8"
                   >
-                    Your email
-                  </label>
-                  <input
-                    type="email"
-                    id="email"
-                    class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                    placeholder="name@flowbite.com"
-                    required
-                  />
+                    <div>
+                      <label
+                        for="email"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                      >
+                        Your email
+                      </label>
+                      <input
+                        type="email"
+                        id="email"
+                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                        placeholder="email..."
+                        required
+                      />
+                    </div>
+                    <div>
+                      <label
+                        for="subject"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                      >
+                        Subject
+                      </label>
+                      <input
+                        type="text"
+                        id="subject"
+                        class=" block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                        placeholder="Let me know how I can help you..."
+                        required
+                      />
+                    </div>
+                    <div class="sm:col-span-2">
+                      <label
+                        for="message"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+                      >
+                        Your message
+                      </label>
+                      <textarea
+                        id="message"
+                        rows="6"
+                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        placeholder="Leave a comment..."
+                      ></textarea>
+                    </div>
+                    <div className="text-center">
+                      <button
+                        type="submit"
+                        class="text-center  my-4 text-myGrey  border border-myWine focus:outline-none hover:bg-gray-200 focus:ring-4 focus:ring-gray-200 font-medium rounded-2xl text-sm px-5 py-2.5   dark:bg-gray-800 dark:text-white dark:border-myWine dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      >
+                        Send message
+                      </button>
+                    </div>
+                  </form>
                 </div>
-                <div>
-                  <label
-                    for="subject"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                  >
-                    Subject
-                  </label>
-                  <input
-                    type="text"
-                    id="subject"
-                    class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                    placeholder="Let us know how we can help you"
-                    required
-                  />
-                </div>
-                <div class="sm:col-span-2">
-                  <label
-                    for="message"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                  >
-                    Your message
-                  </label>
-                  <textarea
-                    id="message"
-                    rows="6"
-                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    placeholder="Leave a comment..."
-                  ></textarea>
-                </div>
-                <button
-                  type="submit"
-                  class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-                >
-                  Send message
-                </button>
-              </form>
+              </section>
             </div>
-          </section>
-
           </div>
-
-         
-        </div>
-          
         </div>
       </div>
     </div>
-  );
-};
-export default Contact;
+  )
+}
+export default Contact
