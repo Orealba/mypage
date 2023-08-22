bc65a6394af832992bdac27b2f60e19ba8344eb1
Orealba Soriano <orealbas@gmail.com>
2023-08-22 13:19:32 +0200

    added the photo on the contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import GitIcon from '../images/git.svg'
-import LinkIcon from '../images/link.svg'
+import Form from "./Form"
+
+import ContactPhoto from "../images/ContactPhoto.jpg"
 
 const Contact = () => {
   return (
@@ -10,79 +11,21 @@ const Contact = () => {
           <h4 className="text-center pt-10 font-semibold  sm:text-5xl text-myGrey">
             Shall we have a coffee?
           </h4>
-          <div className="border-solid border-2 border-myWine my-12 mx-12">
-            <div className=" ">
-              <section class=" bg-white dark:bg-gray-900">
-                <div class=" lg:py-16 px-60 mx-auto max-w-screen-md">
-                  <form
-                    action="#"
-                    class="border-solid border-2 border-myWine py-8 px-8 space-y-8"
-                  >
-                    <div>
-                      <label
-                        for="email"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                      >
-                        Your email
-                      </label>
-                      <input
-                        type="email"
-                        id="email"
-                        class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                        placeholder="email..."
-                        required
-                      />
-                    </div>
-                    <div>
-                      <label
-                        for="subject"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                      >
-                        Subject
-                      </label>
-                      <input
-                        type="text"
-                        id="subject"
-                        class=" block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                        placeholder="Let me know how I can help you..."
-                        required
-                      />
-                    </div>
-                    <div class="sm:col-span-2">
-                      <label
-                        for="message"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-                      >
-                        Your message
-                      </label>
-                      <textarea
-                        id="message"
-                        rows="6"
-                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                        placeholder="Leave a comment..."
-                      ></textarea>
-                    </div>
-                    <div className="text-center">
-                      <button
-                        type="submit"
-                        class="text-center  my-4 text-myGrey  border     border-myWine focus:outline-none hover:bg-gray-200 focus:ring-4 focus:ring-gray-200 font-medium rounded-2xl text-sm px-5 py-2.5   dark:bg-gray-800 dark:text-white dark:border-myWine dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-                      >
-                        Send message
-                      </button>
-                    </div>
-                    <div className="flex">
-                      <button type="submit">
-                        <img src={GitIcon}></img>
-                      </button>
-                      <button type="submit">
-                        <img src={LinkIcon}></img>
-                      </button>
-                    </div>
-                  </form>
-                </div>
-              </section>
+          <div className="flex">
+
+            <div className='border-2 border-myWine my-12' >
+              <div>
+                <Form />
+              </div>
+
             </div>
-            <div></div>
+            <div>
+              <img src={ContactPhoto} className=" my-12" />
+            </div>
+          </div>
+
+          <div>
+
           </div>
         </div>
       </div>
---
+++ src/components/Form.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import GitIcon from '../images/git.svg'
+import LinkIcon from '../images/link.svg'
+
+const Form = () => {
+  return (
+    <div>
+      <section className=" bg-white dark:bg-gray-900">
+        <div className="py-8 lg:py-16 px-40 mx-auto max-w-screen-md">
+                       
+          <form
+            action="#"
+            className="border-2 border-myWine py-8 px-32  space-y-8 "
+          >
+            <div>
+              <label
+                for="email"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className=" shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                          
+                
+                placeholder="email..."
+                required
+              />
+            </div>
+            <div>
+              <label
+                for="subject"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                id="subject"
+                className=" block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Let me know how I can help you..."
+                required
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                for="message"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Your message
+              </label>
+              <textarea
+                id="message"
+                rows="6"
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Leave a comment..."
+              ></textarea>
+            </div>
+            <div className="text-center">
+              <button
+                type="submit"
+                className="text-center  my-4 text-myGrey  border     border-myWine focus:outline-none hover:bg-gray-200 focus:ring-4 focus:ring-gray-200 font-medium rounded-2xl text-sm px-5 py-2.5   dark:bg-gray-800 dark:text-white dark:border-myWine dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+              >
+                Send message
+              </button>
+            </div>
+            <div className="flex">
+              <button type="submit">
+                <img src={GitIcon}></img>
+              </button>
+              <button type="submit">
+                <img src={LinkIcon}></img>
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  )
+
+}
+export default Form
