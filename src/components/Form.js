0f3070f8671a154fcccc7c51fa7ea1ae8f533288
Orealba Soriano <orealbas@gmail.com>
2023-08-24 10:47:01 +0200

    changing the form again

--- src/components/Form.js
+++ src/components/Form.js
@@ -22,7 +22,7 @@ const Form = () => {
 
    <div>
       <section className=" bg-white dark:bg-gray-900">
-        <div className="py-8 lg:py-16 px-40 mx-auto max-w-screen-md">
+        <div className="py-8 lg:py-16 px-20 mx-auto max-w-screen-md">
 
           <form
           ref={form} onSubmit={sendEmail}
@@ -40,7 +40,7 @@ const Form = () => {
               name="user_email"
                 type="email"
                 id="email"
-                className=" shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
 
 
                 placeholder="email..."
