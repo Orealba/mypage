a77102c3bc7cb16133ef6289b64583a3cf3d758f
Orealba Soriano <orealbas@gmail.com>
2023-08-28 17:53:16 +0200

    chenges in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
     <div>
-      <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4 ">
-        <div class="flex w-full max-w-screen-xl mx-auto p-4 md:py-8 ">
+      <footer class="  m-4 ">
+        <div class="flex justify-between w-full max-w-screen-xl mx-auto p-4 md:py-8  ">
           <span class="block text-sm text-gray-500 sm:text-start dark:text-gray-400 pr-2">
             © 2023{' '}
             <a href="https://flowbite.com/" class="hover:underline">
