27197a49351e5d8b4647dcda14604a8a1c1d4187
Orealba Soriano <orealbas@gmail.com>
2025-11-03 16:05:34 +0100

    finish the journalist jobs

--- src/components/Redes/Redes.js
+++ src/components/Redes/Redes.js
@@ -10,7 +10,7 @@ const Redes = () => {
       className="bg-mybG shadow-myShadow">
       <div className="container mx-auto ">
         <h4 className="pl-8 pb-5 mt-8 sm:text-2xl font-semibold text-myGrey">
-          Redes Sociales
+          MY SOCIAL MEDIA
         </h4>
 
         {/* Fila: texto izquierda, imagen derecha */}
@@ -19,8 +19,24 @@ const Redes = () => {
             <div className="md:w-1/2 text-md">
               <p className="text-justify pb-5">
                 {/* Reemplaza este texto por el de tu proyecto Redes Sociales */}
-                Aquí va la descripción de tu proyecto de Redes Sociales. Puedes
-                detallar objetivos, tecnologías y resultados.
+                A few months ago, I started{' '}
+                <strong>Ore's Tennis Journal </strong>, my personal Instagram
+                project where I combine <strong> my passion for tennis</strong>{' '}
+                with my
+                <strong> creative side</strong>. Here, I am not only the boss
+                but also the
+                <strong>
+                  {' '}
+                  director, producer, and editor: I record, edit, and create all
+                  the content myself
+                </strong>
+                . Little by little, I am building a community of tennis fans
+                like me, sharing news, analysis, and curiosities about the sport
+                I love. Every post is a small step of learning and growth, where
+                I apply both technical skills and sports passion{' '}
+                <strong>to keep the audience connected and entertained</strong>.
+                It's an experiment full of drive and eagerness to keep
+                improving!
               </p>
             </div>
             <div className="md:w-1/2">
@@ -44,7 +60,7 @@ const Redes = () => {
               <button
                 type="button"
                 className="my-10 text-myGrey border border-myWine focus:outline-none hover:bg-gray-200 focus:ring-4 focus:ring-gray-200 font-medium rounded-2xl text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-myWine dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
-                Redes Sociales
+               Ore's Tennis Journal
               </button>
             </a>
           </div>
